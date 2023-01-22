0e3466d12cff7dff960a99e5ed215777b45939f9
Egidijus <egidijus.baltutis@gmail.com>
2023-01-22 16:19:37 +0200

    import

---
+++ Python Tasks/Task 1/main.py
@@ -0,0 +1,21 @@
+# Turimas "users" masyvas. 
+
+# Parašykite funkciją "filterAdultDogOwers", kuri atlikas nurodytas veiksmsus:
+# 1. Kaip argumentą priims masyvą
+# 2. Grąžins pilnamečius "users", kurie turi augintinį.
+
+users = [
+  { "id": '1', "name": 'John Smith', "age": 20, "hasDog": True },
+  { "id": '2', "name": 'Ann Smith', "age": 24, "hasDog": False },
+  { "id": '3', "name": 'Tom Jones', "age": 31, "hasDog": True },
+  { "id": '4', "name": 'Rose Peterson', "age": 17, "hasDog": False },
+  { "id": '5', "name": 'Alex John', "age": 25, "hasDog": True },
+  { "id": '6', "name": 'Ronald Jones', "age": 63, "hasDog": True },
+  { "id": '7', "name": 'Elton Smith', "age": 16, "hasDog": True },
+  { "id": '8', "name": 'Simon Peterson', "age": 30, "hasDog": False },
+  { "id": '9', "name": 'Daniel Cane', "age": 51, "hasDog": True },
+]
+
+
+def filterAdultDogOwers(users):
+  pass
---
+++ Python Tasks/Task 2/main.py
@@ -0,0 +1,27 @@
+# Turimas "users" masyvas. 
+
+# Parašykite funkcijas, kurios atlikas nurodytas užduotis:
+# 1. funkcija "getUserMedianAge" - kaip argumentą priims masyvą ir duoto masyvo 
+# atveju grąžins visų "users" amžiaus medianą.
+# 2. funkcija "getOldestUser" -  kaip argumentą priims masyvą ir duoto masyvo 
+# atveju grąžins vyriausio vartotojo vardą.
+
+users = [
+  { "id": '1', "name": 'John Smith', "age": 20 },
+  { "id": '2', "name": 'Ann Smith', "age": 24 },
+  { "id": '3', "name": 'Tom Jones', "age": 31 },
+  { "id": '4', "name": 'Rose Peterson', "age": 17 },
+  { "id": '5', "name": 'Alex John', "age": 25 },
+  { "id": '6', "name": 'Ronald Jones', "age": 63 },
+  { "id": '7', "name": 'Elton Smith', "age": 16 },
+  { "id": '8', "name": 'Simon Peterson', "age": 30 },
+  { "id": '9', "name": 'Daniel Cane', "age": 51 },
+]
+
+
+def getUserMedianAge(users):
+  pass
+
+
+def getOldestUser(users):
+  pass
